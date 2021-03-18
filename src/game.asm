cde39257bd9ccae771326621bbdcf7bb6d20be36
aquila_zyy <aquila.zyy@gmail.com>
2021-03-18 11:38:43 +0800

    Draw entire plane

--- src/game.asm
+++ src/game.asm
@@ -52,14 +52,16 @@ ENEMY:	.byte	0:6	# struct enemy {
 .eqv	PLAY_ADDRESS	0x1000c410	# The top left of the actual game
 .eqv	WIDTH		128
 .eqv	HEIGHT		128
+.eqv	WIDTH_ADDR	512
 	# Colors
 .eqv	WHITE		0x00ffffff
-.eqv	RED		0x00ff0000
-.eqv	YELLOW		0x00ffff00
+.eqv	RED		0x00ac3232
+.eqv	YELLOW		0x00fbf236
 .eqv	GREEN		0x0000ff00
 .eqv	CYAN		0x0000ffff
 .eqv	BLUE		0x000000ff
 .eqv	VIOLET		0x00ff00ff
+.eqv	DARK_GREY		0x0045283c
 
 .text
 .globl main
@@ -87,6 +89,11 @@ main:	# Initialize the game
 	li $a2, HEIGHT
 	jal draw_vert	# Draw right border
 	
+	li $a0, 5
+	li $a1, 79
+	li $a2, -1
+	li $a3, -1
+	jal draw_plane
 	
 	# Terminate
 	li $v0, 10
@@ -118,24 +125,33 @@ draw_vert:
 	move $t8, $ra	# Temporarily move $ra for nested function calls
 	jal coor_to_addr
 	move $ra, $t8	# Move $ra back
-	sll $t9, $t9, 2	# $t9 is WIDTH, shift left 2 so that it is now 
-			# address difference between rows
 	
 dv_do:	sw $a3, 0($v0)	# Paint color to the map
 	addi $a2, $a2, -1	# k--
-	add $v0, $v0, $t9	# Advance pointer
+	addi $v0, $v0, WIDTH_ADDR	# Advance pointer
 	bgtz $a2, dv_do	# if k>0, loop
 	
 	jr $ra	# return
 	
+# This function draws a horizontal line starting from some address x and k pixel right, with some 
+# color c.
+# x, k, c, is passed in with $a0, $a2, $a3 (!)
+# This function does not modify anything besides $a0, $a2.
+draw_hori_addr:
+dha_do:	sw $a3, 0($a0)	# Paint color to the map
+	addi $a2, $a2, -1	# k--
+	addi $a0, $a0, 4	# Advance pointer
+	bgtz $a2, dha_do	# if k>0, loop
+	
+	jr $ra	# return
+
 # This function converts a set of coordinates (x, y) to a memory address on the bit map, 
 # where x, y are passed in from $a0 and $a1, and the return value is stored in $v0.
 # This function modifies $a0, $a1, $v0, and $t9;
 # This function does not call other functions.
 coor_to_addr:
-	li $t9, WIDTH
-	mul $a1, $a1, $t9
-	sll $a1, $a1, 2	# Amount of memory we shift downward
+	li $t9, WIDTH_ADDR
+	mul $a1, $a1, $t9	# Amount of memory we shift downward
 	sll $a0, $a0, 2	# Amount of memory we shift right
 	add $a0, $a0, $a1	# Total memory shift from base_address
 	addi $v0, $a0, BASE_ADDRESS	# Actual memory address
@@ -145,5 +161,71 @@ coor_to_addr:
 # x, y, old_x, old_y and uses register calling convention. Set old_x or old_y to -1 to 
 # draw an entire plane. Otherwise, (x, y) and (old_x, old_y) should be neighbouring 
 # pixels and this function only draws the difference between two frames.
-# This function modifies $a0, $a1, $t8, $t9.
+# This function modifies a lot of things, do not assume any perservation.
 draw_plane:
+	move $t8, $ra	# Move $ra away, this function calls many other functions
+	# Get central address
+	jal coor_to_addr
+	# Load color values
+	li $t0, DARK_GREY
+	li $t1, RED
+	li $t2, YELLOW
+	# Draw entire plane if old_x or old_y is negative.
+	bltz $a2, drawp_whole
+	bltz $a3, drawp_whole
+	
+	
+drawp_whole:
+	# Draw cockpit
+	sw $t0 0($v0)
+	sw $t0 4($v0)
+	# Draw nose
+	sw $t1, 8($v0)
+	sw $t1, 12($v0)
+	sw $t1, 16($v0)
+	# Draw behind cockpit
+	sw $t1, -8($v0)
+	sw $t1, -4($v0)
+	# Draw right shoulder
+	addi $a0, $v0, WIDTH_ADDR
+	addi $a0, $a0, -8
+	li $a2, 5
+	li $a3, RED
+	jal draw_hori_addr
+	# Draw right pulser
+	addi $a0, $a0, WIDTH_ADDR
+	addi $a0, $a0, -24
+	sw $t2, -4($a0)	# Draw that yellow dot
+	li $a2, 4
+	jal draw_hori_addr
+	# Draw right wing remaining
+	addi $a0, $a0, WIDTH_ADDR
+	sw $t1, -12($a0)
+	sw $t1, -8($a0)
+	addi $a0, $a0, WIDTH_ADDR
+	sw $t1, -12($a0)
+	# Draw left shoulder
+	addi $a0, $v0, -WIDTH_ADDR
+	addi $a0, $a0, -8
+	li $a2, 5
+	li $a3, RED
+	jal draw_hori_addr
+	# Draw left pulser
+	addi $a0, $a0, -WIDTH_ADDR
+	addi $a0, $a0, -24
+	sw $t2, -4($a0)	# Draw that yellow dot
+	li $a2, 4
+	jal draw_hori_addr
+	# Draw left wing remaining
+	addi $a0, $a0, -WIDTH_ADDR
+	sw $t1, -12($a0)
+	sw $t1, -8($a0)
+	addi $a0, $a0, -WIDTH_ADDR
+	sw $t1, -12($a0)
+	
+	
+	
+	
+drawp_end:
+	jr $t8
+	
